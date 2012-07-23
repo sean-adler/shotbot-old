094b3e0093ab9aaa558d82cd0d28123af8410a83
Sean Adler <sadler13@cmc.edu>
2012-07-22 23:28:23 -0700

    Add concurrent pouring to shotbot_server.py and reword comments

--- flask_server/shotbot_server.py
+++ flask_server/shotbot_server.py
@@ -10,7 +10,15 @@ import time
 # Specify the Uno's port as an argument
 uno = Arduino('/dev/tty.usbmodem621')
 
-# Define which pin is connected to each ingredient
+# Declare all output pins
+
+uno.output([1,2,3,4,5,6,7,8])
+
+"""
+PIN ASSOCIATIONS
+
+Purely for documentation (these vars are never used)
+
 whiskey = 1
 tequila = 2
 vodka = 3
@@ -19,10 +27,7 @@ orange_juice = 5
 pineapple_juice = 6
 cranberry_juice = 7
 sour_mix = 8
-
-# Declare all output pins
-
-uno.output([1,2,3,4,5,6,7,8])
+"""
 
 ################################
 ###    Flask server setup    ###
@@ -33,24 +38,43 @@ app = Flask(__name__)
 
 # Declare the server's only route
 @app.route('/<ingredients>')
-def pour_drink(ingredients):
+
+def pour(ingredients):
     """
-    Takes argument 'ingredients' which is always an 8-digit string of ints.
-    Each number in the string corresponds to how long a particular
-    ingredient is poured (or equivalently, how long its corresponding
-    pin is HIGH).
+    Take the URL string and pass it to our recursive function
+    as a list of ints.
     """
-    for pin in range(len(ingredients)):
-        if ingredients[pin] != "0":
-            # Determine pour time
-            pour_time = float(ingredients[pin])
-            # Turn on appopriate pin -- Arduino pins are NOT zero-offset!
-            uno.setHigh(pin+1)
-            # Leave pin high for appropriate time
-            time.sleep(pour_time)
-            uno.setLow(pin+1)
-    # Print ingredients to browser window
-    return ingredients
+    drinkList = list([int(e) for e in ingredients])
+    conc_helper(drinkList)
+    return ""
+
+def conc_helper(L):
+    """
+    This helper function enables concurrent pouring.
+    Opens all required valves immediately and closes each when necessary.
+    """
+    # Recurse until all ingredients are completely poured
+    if any([e for e in L if e > 0]):
+        for e in range(len(L)):
+            if L[e] > 0 and uno.getState(e+1) == 'LOW':
+                # Arduino pins are not zero-offset,
+                # Add 1 to the list index to access correct pin
+                uno.setHigh(e+1)
+                print "Starting pin %d" % (e+1)
+        # Leave pins on until at least one ingredient is completely poured
+        sleep_time = min([e for e in L if e > 0])
+        time.sleep(sleep_time)
+        # Subtract time elapsed from all ingredients
+        newL = [(e-sleep_time) for e in L]
+        # Turn off pins whose ingredients are completely poured
+        for e in range(len(newL)):
+            if newL[e] == 0:
+                uno.setLow(e+1)
+                print "Stopping pin %d" % (e+1)
+        # Recurse on adjusted list
+        conc_helper(newL)
+    else:
+        return
 
 if __name__ == '__main__':
     app.run()
--- flask_server/test_shotbot_server.py
+++ flask_server/test_shotbot_server.py
@@ -10,8 +10,11 @@ import time
 # Specify the Uno's port as an argument
 #uno = Arduino('/dev/tty.usbmodem621')
 
-# Define which pin is connected to each ingredient
-# (We don't actually use these vars, included solely for documentation.)
+"""
+PIN ASSOCIATIONS
+
+Purely for documentation (these vars are never used)
+
 whiskey = 1
 tequila = 2
 vodka = 3
@@ -20,6 +23,7 @@ orange_juice = 5
 pineapple_juice = 6
 cranberry_juice = 7
 sour_mix = 8
+"""
 
 # Declare all output pins
 
@@ -36,6 +40,10 @@ app = Flask(__name__)
 @app.route('/<ingredients>')
 
 def pour(ingredients):
+    """
+    Take the URL string and pass it to our recursive function
+    as a list of ints.
+    """
     drinkList = list([int(e) for e in ingredients])
     conc_helper(drinkList)
     return ""
@@ -43,17 +51,26 @@ def pour(ingredients):
 # Helper function: Allows concurrent pouring
 # Open all required valves at once and close when necessary
 def conc_helper(L):
+    """
+    This helper function enables concurrent pouring.
+    Opens all required valves immediately and closes each when necessary.
+    """
+    # Recurse until all ingredients are completely poured
     if any([e for e in L if e > 0]):
         for e in range(len(L)):
             if L[e] > 0:
-                # if uno.getState(e+1) != 'HIGH":
+                # Arduino pins are not zero-offset
+                # Add 1 to the list index to access correct pin
                 print "Starting pin %d" % (e+1)
         sleep_time = min([e for e in L if e > 0])
         time.sleep(sleep_time)
+        # Subtract time elapsed from all ingredients
         newL = [(e-sleep_time) for e in L]
+        # Turn off pins whose ingredients are completely poured
         for e in range(len(newL)):
             if newL[e] == 0:
                 print "Stopping pin %d" % (e+1)
+        # Recurse on adjusted list
         conc_helper(newL)
     else:
         return
